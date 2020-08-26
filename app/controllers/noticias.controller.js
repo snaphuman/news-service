90ee4ab8aba25c9024f71b047d73d7289b918947
snaphuman <fhernandezn@gmail.com>
2020-08-26 12:09:08 -0500

    add magination methods for noticias controller

--- app/controllers/noticias.controller.js
+++ app/controllers/noticias.controller.js
@@ -1,13 +1,35 @@
 const db = require("../models");
 const Noticia = db.noticias;
-console.log(Noticia);
 const Op = db.Sequelize.Op;
+
+const getPagination = (page, size) => {
+    const limit = size ? +size : 10;
+    const offset = page ? page * limit : 0;
+
+    return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+    const { count: totalItems, rows: noticias } = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil(totalItems / limit);
+
+    return { totalItems, noticias, totalPages, currentPage };
+};
 
 exports.findAll = (req, res) => {
 
-    Noticia.findAll({})
+    const { page, size, name } = req.query;
+    console.log(name);
+
+    var condition = name ? {name: {[Op.substring]: name}} : null;
+
+    const { limit, offset } = getPagination(page, size);
+
+    Noticia.findAndCountAll({ where: condition, limit, offset })
         .then(data => {
-            res.send(data);
+            const response = getPagingData(data, page, limit);
+            res.send(response);
         })
         .catch(err => {
             res.status(500).send({
