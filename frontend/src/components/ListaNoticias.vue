8108f8413ace9c4858671e04b28bfd11b0870418
snaphuman <fhernandezn@gmail.com>
2020-08-28 07:18:23 -0500

    [front] implement search filter and pagination

--- frontend/src/components/ListaNoticias.vue
+++ frontend/src/components/ListaNoticias.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <b-form-input v-model="searchText" @change="doSearch()" debounce="500" type="search" placeholder="Ingrese texto de búsqueda"></b-form-input>
   <b-list-group>
     <b-list-group-item v-for="(noticia, index) in noticias" :key="index" v-bind:href="`${getContentURL(noticia.slug)}`" class="flex-column align-items-start">
       <div class="d-flex w-100 justify-content-between">
@@ -13,19 +14,24 @@
 
     </b-list-group-item>
   </b-list-group>
+  <div class="overflow-auto">
+      <b-pagination-nav v-model="currentPage" :link-gen="linkGen" :number-of-pages="10" :no-page-detect="true"></b-pagination-nav>
+  </div>
 </div>
 </template>
 <script>
 import NoticiasDataService from '../services/NoticiasDataService';
-import { BListGroup, BListGroupItem } from 'bootstrap-vue';
+import { BListGroup, BListGroupItem, FormInputPlugin, BPaginationNav } from 'bootstrap-vue';
 
 
 export default {
     name: 'lista-noticias',
-    components: [ BListGroup, BListGroupItem ],
+    components: [ BListGroup, BListGroupItem, FormInputPlugin, BPaginationNav ],
     data() {
         return {
             noticias: [],
+            searchText: '',
+            currentPage: 1
         }
     },
     methods: {
@@ -38,16 +44,30 @@ export default {
                     console.log(e);
                 });
         },
+        async doSearch() {
+           return await NoticiasDataService.getByKeyword(this.searchText)
+                .then(response => {
+                    this.noticias = response.data.noticias;
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
         getContentURL(slug) {
             return "https://www.crcom.gov.co/es/noticia/"+slug;
-        }
-    },
-    computed: {
-
+        },
+        linkGen() {
+            NoticiasDataService.setPage(this.currentPage - 1)
+                .then(response => {
+                    this.noticias = response.data.noticias;
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
     },
     mounted() {
         this.getNoticias();
     }
 }
-
 </script>
--- frontend/src/services/NoticiasDataService.js
+++ frontend/src/services/NoticiasDataService.js
@@ -4,6 +4,23 @@ class NoticiasDataService {
     getAll() {
         return http.get('/noticias');
     }
+
+    getByKeyword(text) {
+        return http.get('/noticias', {
+            params: {
+                name: text,
+                all: true
+            }
+        });
+    }
+
+    setPage(pageNum) {
+        return http.get('/noticias', {
+            params: {
+                page: pageNum
+            }
+        });
+    }
 }
 
 export default new NoticiasDataService();
